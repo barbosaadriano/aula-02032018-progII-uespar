13a29ad16c9f326d000dc40d673db9646d30527b
Adriano Barbosa <b.adrianobarbosa@gmail.com>
2018-03-01 22:32:32 -0300

    projeto finalizado Aula 01/03/2018

---
+++ Aula.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Aula {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int idade;
+		System.out.println("Escreve sua idade ai p@R#!");
+		idade = scan.nextInt();
+		System.out.println( "Sua idade é " + idade );
+		
+	}
+	
+}
